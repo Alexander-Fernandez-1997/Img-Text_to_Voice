415402272f3cc1eab8c569f504a5e755452c5b3b
KingAlex1997 <alex97l@hotmail.com>
2022-03-01 01:10:05 -0300

    app para leer imagenes con texto o puro texto

---
+++ script.js
@@ -0,0 +1,126 @@
+const textarea = document.querySelector("textarea"),
+  voiceList = document.querySelector("select"),
+  speechBtn = document.querySelector("button");
+
+let synth = speechSynthesis,
+  isSpeaking = true;
+
+const input = document.querySelector("input");
+const preview = document.querySelector(".preview");
+
+input.addEventListener("change", updateImageDisplay);
+
+function updateImageDisplay() {
+  while (preview.firstChild) {
+    preview.removeChild(preview.firstChild);
+  }
+
+  const curFiles = input.files;
+  if (curFiles.length === 0) {
+    const para = document.createElement("p");
+    para.textContent = "No files currently selected for upload";
+    preview.appendChild(para);
+  } else {
+    const list = document.createElement("ol");
+    preview.appendChild(list);
+
+    for (const file of curFiles) {
+      const listItem = document.createElement("li");
+      const para = document.createElement("p");
+      if (validFileType(file)) {
+        para.textContent = "Loading the info, pls wait";
+        listItem.appendChild(para);
+        const image = document.createElement("img");
+        image.src = URL.createObjectURL(file);
+
+        (async () => {
+          const worker = Tesseract.createWorker();
+          await worker.load();
+          await worker.loadLanguage("eng");
+          await worker.initialize("eng");
+          const {
+            data: { text },
+          } = await worker.recognize(image.src);
+
+          await worker.terminate();
+
+          textarea.value = text;
+        })();
+      } else {
+        para.textContent = `File name ${file.name}: Not a valid file type. Update your selection.`;
+        listItem.appendChild(para);
+      }
+
+      list.appendChild(listItem);
+    }
+  }
+}
+
+// https://developer.mozilla.org/en-US/docs/Web/Media/Formats/Image_types
+const fileTypes = [
+  "image/apng",
+  "image/bmp",
+  "image/gif",
+  "image/jpeg",
+  "image/pjpeg",
+  "image/png",
+  "image/svg+xml",
+  "image/tiff",
+  "image/webp",
+  "image/x-icon",
+];
+
+function validFileType(file) {
+  return fileTypes.includes(file.type);
+}
+
+voices();
+
+function voices() {
+  for (let voice of synth.getVoices()) {
+    let selected = voice.name === "Google US English" ? "selected" : "";
+    let option = `<option value="${voice.name}" ${selected}>${voice.name} (${voice.lang})</option>`;
+    voiceList.insertAdjacentHTML("beforeend", option);
+  }
+}
+
+synth.addEventListener("voiceschanged", voices);
+
+function textToSpeech(text) {
+  let utterance = new SpeechSynthesisUtterance(text);
+  for (let voice of synth.getVoices()) {
+    if (voice.name === voiceList.value) {
+      utterance.voice = voice;
+    }
+  }
+  synth.speak(utterance);
+}
+
+speechBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (textarea.value !== "") {
+    if (!synth.speaking) {
+      textToSpeech(textarea.value);
+    }
+    if (textarea.value.length > 80) {
+      setInterval(() => {
+        if (!synth.speaking && !isSpeaking) {
+          isSpeaking = true;
+          speechBtn.innerText = "Convert To Speech";
+        } else {
+        }
+      }, 500);
+      if (isSpeaking) {
+        synth.resume();
+        isSpeaking = false;
+        speechBtn.innerText = "Pause Speech";
+      } else {
+        synth.pause();
+        isSpeaking = true;
+        speechBtn.innerText = "Resume Speech";
+      }
+    } else {
+      speechBtn.innerText = "Convert To Speech";
+    }
+  }
+});
